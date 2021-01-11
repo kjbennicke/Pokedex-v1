b1ba2ef461e1dbe7e26cfdd1c61f6e9bc5b08c65
kjbennicke <kjbennicke@gmail.com>
2021-01-11 13:13:17 -0800

    Setup partials and added fil structure to app.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // Initial express setup
 const express = require('express');
 const app = express();
+const colors = require('colors');
+const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 
 //ROUTES
-app.get('/', (req, res)=>{
-    res.render('home');
+app.get('/', (req, res) => {
+  res.render('home');
 });
-app.get('*', (req, res)=>{
-    console.error('This is not a valid route.');
-    res.render('error');
+app.get('*', (req, res) => {
+  console.error('This is not a valid route.');
+  res.render('error');
 });
 
 //Server port
-app.listen(3000, ()=>{
-    console.log("Pokedex Booted!")
+app.listen(3000, () => {
+  console.log('Pokedex Booted!'.cyan.underline);
 });
